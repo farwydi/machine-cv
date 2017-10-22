fe860ee86f9e84041d87cc5aaf48b0af6c8c4c37
Leonid Zharikov <leon.zharikov@gmail.com>
2017-10-22 14:41:02 +0300

    build true

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,11 +7,11 @@ class TestMain(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
-        self.assertTrue('FO'.isupper())
+        self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
-        s = 'helloworld'
+        s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
